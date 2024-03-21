bd28e8d54578f3a5d42bebed6372d570bcc3600c
Almaz <nazarovalmaz12@gmail.com>
2024-03-21 20:08:35 +0600

    Growmodo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// var swiper = new Swiper(".get__swiper", {
+//     slidesPerView: 2,
+//     spaceBetween: 30,
+//     freeMode: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
